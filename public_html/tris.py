9fd87ed19f6a694de69ab958bf1c0a4e5538b0fc
Michele Monteferrante <michele.monteferrante@gmail.com>
2023-03-29 12:57:59 +0200

    mattina 29/03

--- public_html/tris.py
+++ public_html/tris.py
@@ -1,6 +1,5 @@
 import os
-def clear():
-    os.system("cls")
+from public_html.tris_lib import trislib
 # -----------
 from flask import Flask
 from markupsafe import escape
@@ -10,7 +9,7 @@ from flask import abort, redirect, url_for
 
 app = Flask(__name__)
 
-
+#Redirect
 @app.route("/")
 def index():
     return redirect(url_for('home'))
@@ -20,6 +19,32 @@ def index():
 def home():
     return render_template("homepage.html")
 
+#API
+@app.route('/new_room', methods = ['GET'])
+def __init__():
+    #Inizializza la stanza 
+    #
 
+    return redirect(f'/{codiceStanza}/{username}')
+
+@app.route('/join/<str:codiceStanza>', methods = ['GET'])
+def join():
+    #Fa in modo di entrare in una stanza già esistente
+    #controllo sul numero di utenti nella stanza 
+    #<2 accedi alla stanza come giocatore
+    #>2 accedi alla stanza come spettatore
+    return redirect('/codice_stanza/n_utente')
+
+@app.route('/<str:codiceStanza>/<str:username>', methods = ['POST'])
+def mossa():
+    #controlla la mossa effettuata
+    #salva la mossa se è valida
+    #scambia la variabile del turno
+    return
+@app.route('/<str:codiceStanza>', methods = ['GET'])
+def getStatus():
+    #Ritorna un array con lo stato della partita(casselle occupate, di chi è il turno)
+    
+    return 
 # -----------
-clear()
+trislib.clearTerminal()
